6898352399ea26dd8fb8a068ecc97ba5f958831f
Apollo Tang <tang.apollo@gmail.com>
2016-10-19 16:46:15 -0400

    add part 2 of chapter 2 (adding more screens)

---
+++ lessons/02-rendering-a-route--part2-adding-more-screens/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    {/* http://localhost:8080/#/repos */}
+    <Route path="/repos" component={Repos}/>
+    {/* http://localhost:8080/#/about  */}
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'));
+
